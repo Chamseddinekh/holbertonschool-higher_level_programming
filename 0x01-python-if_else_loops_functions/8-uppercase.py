18d74443387d08b916de4e5b28a6d7213502c77a
Chamseddinekh <chamsseddine.khedhri@gmail.com>
2020-04-28 23:59:49 +0100

    uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for i in str:
+        if ord(i) > 96 and ord(i) < 123:
+            i = ord(i) - 32
+            print("{}".format(chr(i)), end="")
+    print("")
