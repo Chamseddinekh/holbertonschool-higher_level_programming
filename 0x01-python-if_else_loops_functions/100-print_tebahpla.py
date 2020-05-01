65c8236f961683fd246031792925a89cc2980d4b
Chamseddinekh <chamsseddine.khedhri@gmail.com>
2020-05-01 04:37:36 +0100

    fixed

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for i in range(122, 96, -1):
     if i % 2 == 0:
-        i = chr(i)
+        chr(i)
     else:
-        i = chr(i-32)
-    print(i, end="")
+        chr(i-32)
+    print(chr(i), end="")
