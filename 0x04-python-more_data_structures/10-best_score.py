2aeee444ba790dc324393e73e1a480f84beb9de5
Chamseddinekh <chamsseddine.khedhri@gmail.com>
2020-05-07 23:22:26 +0100

    max value

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,6 +2,6 @@
 def best_score(a_dictionary):
     if not a_dictionary:
         return None
-    value=list(a_dictionary.values())
-    key=list(a_dictionary.keys())
+    value = list(a_dictionary.values())
+    key = list(a_dictionary.keys())
     return key[value.index(max(value))]
