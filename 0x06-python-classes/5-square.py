61e695b22b5ee461562ac3f944b08b12fde9eec0
Chamseddinekh <chamsseddine.khedhri@gmail.com>
2020-05-21 23:14:30 +0100

    print

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+Define class Square
+"""
+
+
+class Square:
+    """
+    Private instance: size
+    """
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        self.__size = value
+        if not isinstance(self.__size, int):
+            raise TypeError("size must be an integer")
+        if self.__size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        return pow(self.__size, 2)
+
+    def my_print(self):
+        if self.__size > 0:
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print('#', end='')
+                print('')
+        else:
+            print('')
