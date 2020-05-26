78d894efaf98d5a15a2547e7710498afa8717bbf
Chamseddinekh <chamsseddine.khedhri@gmail.com>
2020-05-26 01:35:32 +0100

    del

---
+++ 0x08-python-more_classes/5-rectangle.py
@@ -0,0 +1,78 @@
+#!/usr/bin/python3
+"""
+class Rectangle that defines a rectangle
+"""
+
+
+class Rectangle:
+    """Defines Rectangle Class"""
+    def __init__(self, width=0, height=0):
+        """
+        constructeur
+        """
+        self.height = height
+        self.width = width
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """
+        setter width
+        """
+        if not isinstance(value, int):
+            raise TypeError('width must be an integer')
+        if value < 0:
+            raise ValueError('width must be >= 0')
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """
+        setter heigth
+        """
+        if not isinstance(value, int):
+            raise TypeError('height must be an integer')
+        if value < 0:
+            raise ValueError('height must be >= 0')
+        self.__height = value
+
+    def area(self):
+        """
+        Area of rectangle
+        """
+        return self.height * self.width
+
+    def perimeter(self):
+        """
+        perimetre of rectangle
+        """
+        if self.height == 0 or self.width == 0:
+            return 0
+        return 2 * (self.width + self.height)
+
+    def __str__(self):
+        """
+        print rectangle
+        """
+        string = ''
+        if self.height == 0 or self.width == 0:
+            return string
+        for i in range(self.height):
+            for j in range(self.width):
+                string += '#'
+            if i == self.height - 1:
+                break
+            string += '\n'
+        return string
+
+    def __repr__(self):
+        return ("Rectangle({}, {})".format(self.width, self.height))
+    def __del__(self):
+        print ("Bye rectangle...")
