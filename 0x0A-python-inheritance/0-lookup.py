5ce19d82fce1aa07fa588d8f4ac0f99a4e5a7ec5
Chamseddinekh <chamsseddine.khedhri@gmail.com>
2020-08-20 10:19:22 +0100

    listobj

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+function that returns the list of available attributes
+and methods of an object:
+"""
+
+
+def lookup(obj):
+    """
+    returns the list of available attributes and methods of an object:
+    """
+    return vars(obj)
