1d792422a35d25d79957cd0f4e86d5d6a8dc980f
Chamseddinekh <chamsseddine.khedhri@gmail.com>
2020-08-22 13:26:56 +0100

    integervalidate

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+class BaseGeometry (based on 5-base_geometry.py)
+"""
+
+
+class BaseGeometry():
+    """
+    class BaseGeometry (based on 5-base_geometry.py)
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
