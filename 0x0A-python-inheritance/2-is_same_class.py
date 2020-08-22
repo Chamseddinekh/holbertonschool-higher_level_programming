57906fa91f9cd181b1dd0c31c0807ac914114a3b
Chamseddinekh <chamsseddine.khedhri@gmail.com>
2020-08-22 11:02:01 +0100

    type

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+function that returns True if the object
+is exactly an instance of the specified
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+    function that returns True if the object
+    is exactly an instance of the specified
+    """
+    if type(obj) is a_class:
+        return True
+    return False
