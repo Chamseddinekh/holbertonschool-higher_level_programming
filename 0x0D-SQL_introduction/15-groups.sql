75cb04efbf9c2bf38cab38e005bb1cb865fcb207
chamseddine <chamseddine.khedhri@gmail.com>
2020-12-03 18:08:11 +0100

    groupby

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,3 +1,5 @@
 -- show databases
 -- because Batch 3 is the best!
-SELECT score, COUNT(*) AS number FROM second_table ORDER BY score DESC;
+SELECT score, COUNT(*) AS number FROM second_table
+GROUP BY score
+ORDER BY score DESC;
