0ad1590c942627938299ea20d750db54a83ae724
chamseddine <chamseddine.khedhri@gmail.com>
2020-12-03 17:56:21 +0100

    group

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,3 +1,3 @@
 -- show databases
 -- because Batch 3 is the best!
-SELECT score, COUNT(*) AS number FROM second_table ORDER BY DESC;
+SELECT score, COUNT(*) AS number FROM second_table ORDER BY score DESC;
