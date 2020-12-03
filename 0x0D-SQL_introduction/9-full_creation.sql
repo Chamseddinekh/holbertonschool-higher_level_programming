1ab2268f53cdde6c7464cc0a5613b1726083a33f
chamseddine <chamseddine.khedhri@gmail.com>
2020-12-03 14:50:49 +0100

    createinsert

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,6 +1,10 @@
 -- show databases
 -- because Batch 3 is the best!
-CREATE TABLE IF NOT EXISTS second_table (id INT,name VARCHAR(256),score INT);
+CREATE TABLE IF NOT EXISTS second_table (
+    id INT,
+    name VARCHAR(256),
+    score INT
+    );
 INSERT INTO second_table VALUES
 (1,"John",10),
 (2,"Alex",3),
