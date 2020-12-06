746c1011a226c9600904e17213a3829a57ddf851
chamseddine <chamseddine.khedhri@gmail.com>
2020-12-06 02:11:52 +0100

    write

--- 0x0B-python-input_output/3-write_file.py
+++ 0x0B-python-input_output/3-write_file.py
@@ -6,7 +6,7 @@ function that writes a string to a text file
 
 def write_file(filename="", text=""):
     """
-   function that writes a string to a text file
+    function that writes a string to a text file
     """
-     with open(filename, 'w', encoding='utf-8') as f:
-         return f.write(text)
+    with open(filename, 'w', encoding='utf8') as f:
+        return f.write(text)
