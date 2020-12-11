967dd38bb8bce60b5b064f6c873e7eeb780bf9d9
chamseddine <chamseddine.khedhri@gmail.com>
2020-12-11 14:48:18 +0100

    create

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
 -- SQL - More queries 
 -- creates the MySQL server user user_0d_1
-CREATE USER IF NOT EXISTS 'newuser'@'user_0d_1' IDENTIFIED BY 'user_0d_1_pwd';
+CREATE USER IF NOT EXISTS GRANT ALL PRIVILEGES ON *.* TO 'newuser'@'user_0d_1' IDENTIFIED BY 'user_0d_1_pwd';
