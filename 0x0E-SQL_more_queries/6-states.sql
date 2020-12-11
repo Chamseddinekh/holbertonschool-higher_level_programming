05b17353dfa3d4ef935a2a6afa77cf8b27339a1e
chamseddine <chamseddine.khedhri@gmail.com>
2020-12-11 16:31:25 +0100

    states

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,8 +1,8 @@
 -- SQL - More queries 
 -- creates the MySQL server user user_0d_1
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS [hbtn_0d_usa] states (
-    id INT NOT NULL AUTO_INCREMENT PRIMERY KEY,
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
+    PRIMERY KEY id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(256) NOT NULL,
     UNIQUE KEY (id)
 )
