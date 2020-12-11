04f049aad6e54091dea2818fbd6f10d63caafd13
chamseddine <chamseddine.khedhri@gmail.com>
2020-12-11 16:11:10 +0100

    uniquefixed

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,6 +1,6 @@
 -- SQL - More queries 
 -- creates the MySQL server user user_0d_1
-CREATE TABLE IF NOT EXISTS id_not_null   (
+CREATE TABLE IF NOT EXISTS unique_id   (
     id INT DEFAULT 1,
     name VARCHAR(256),
     UNIQUE KEY (id)
