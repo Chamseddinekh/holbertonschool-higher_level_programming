d6e44d558dd11c0c88f9b04b05a6759b32b0a8c1
chamseddine <chamseddine.khedhri@gmail.com>
2020-12-11 15:09:51 +0100

    user2

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,4 @@
+-- SQL - More queries 
+-- creates the MySQL server user user_0d_1
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
+GRANT SELECT ON *.* TO 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
