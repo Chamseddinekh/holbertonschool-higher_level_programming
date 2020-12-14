ebbb162b1e1cf0427853055c32557c5e84a4775a
chamseddine <chamseddine.khedhri@gmail.com>
2020-12-14 15:52:27 +0100

    j

--- 0x0B-python-input_output/5-to_json_string.py
+++ 0x0B-python-input_output/5-to_json_string.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
+"JSON representation of an object (string):"
 import json
-"SON representation of an object (string):"
 
 
 def to_json_string(my_obj):
+    "JSON representation of an object (string):"
     return json.dumps(my_obj, sort_keys=True)
