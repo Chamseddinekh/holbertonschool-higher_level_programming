c10c541769aeb81411d7bb8133f0e90386a9580a
chamseddine <chamseddine.khedhri@gmail.com>
2020-12-21 22:33:32 +0100

    test

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -23,5 +23,11 @@ class TestMaxInteger(unittest.TestCase):
     def test_all_negative(self):
         self.assertEqual(max_integer([-6, -7, -8]), -6)
 
+    def test_one_elem(self):
+        self.assertEqual(max_integer([6]), 6)
+    
+    def test_max_middle(self):
+        self.assertEqual(max_integer([1, 5, 3]), 5)
+
 if __name__ == '__main__':
     unittest.main()
