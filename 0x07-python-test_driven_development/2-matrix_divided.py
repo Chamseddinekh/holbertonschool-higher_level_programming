64725cbfe7814ba8b3792a953fc61623632238a1
chamseddine <chamseddine.khedhri@gmail.com>
2020-12-21 16:04:21 +0100

    doc

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -5,6 +5,9 @@ function that divides all elements of a matrix.
 
 
 def matrix_divided(matrix, div):
+    """
+    function that divides all elements of a matrix.
+    """
     new_matrix = []
     if type(div) is not int and type(div) is not float:
         raise TypeError("div must be a number")
