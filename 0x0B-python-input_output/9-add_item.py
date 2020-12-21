a5d15ef2825fd945860d940c0eae5267b20ad8b0
chamseddine <chamseddine.khedhri@gmail.com>
2020-12-22 00:59:20 +0100

    add

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -2,7 +2,9 @@
 import sys
 load_from_json_file = __import__("8-load_from_json_file").load_from_json_file
 save_to_json_file = __import__("7-save_to_json_file").save_to_json_file
-"""writes an Object to a text file"""
+"""
+writes an Object to a text file
+"""
 
 filename = "add_item.json"
 try:
