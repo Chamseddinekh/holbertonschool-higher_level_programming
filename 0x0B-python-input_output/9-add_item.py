7591059d360e91d5c40383a7da22e2d512f7170b
chamseddine <chamseddine.khedhri@gmail.com>
2020-12-22 00:51:27 +0100

    add

---
+++ 0x0B-python-input_output/9-add_item.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import sys
+save_to_json_file = __import__("7-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("8-load_from_json_file").load_from_json_file
+"writes an Object to a text file"
+
+
+filename = "add_item.json"
+try:
+    new_list = load_from_json_file(filename)
+except:
+    new_list = []
+for i in range(1, len(sys.argv)):
+    new_list.append(sys.argv[i])
+save_to_json_file(new_list, filename)
