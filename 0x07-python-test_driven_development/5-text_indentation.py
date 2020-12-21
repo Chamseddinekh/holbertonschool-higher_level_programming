1947157bf413385df6528778050c1263a40d1027
chamseddine <chamseddine.khedhri@gmail.com>
2020-12-21 18:30:57 +0100

    ident

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""unction that prints a text with 2 new lines
+   after each of these characters: ., ? and :"""
+
+
+def text_indentation(text):
+    """unction that prints a text with 2 new lines
+       after each of these characters: ., ? and :"""
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+    text = text.replace('.', '.\n\n')
+    text = text.replace('?', '?\n\n')
+    text = text.replace(':', ':\n\n')
+    text = text.split("\n")
+    for i in range(0, len(text)):
+        print("{:s}".format(text[i].strip(), end=""))
