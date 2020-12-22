73559726f14d2157ecd6a7ff0d2770ab829bcfd7
chamseddine <chamseddine.khedhri@gmail.com>
2020-12-22 17:06:59 +0100

    firstcommit

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -13,5 +13,5 @@ class Base:
         if id:
             self.id = id
         else:
-            nb_objects = __nb_objects + 1
-            id = nb_objects
+            __nb_objects += 1
+            id = __nb_objects
