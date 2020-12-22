a83c93fcc2960da077601b8311d48a51d4270d38
chamseddine <chamseddine.khedhri@gmail.com>
2020-12-22 16:52:23 +0100

    reload

---
+++ 0x0B-python-input_output/13-student.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+class Student that defines a student
+"""
+
+
+class Student:
+    """
+    class Student that defines a student
+    """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Public method that retrieves a dictionary
+        representation of a Student instance
+        """
+        if attrs:
+            for i in self.__dict__.items():
+                for j in i:
+                    if j in attrs:
+                        return dict([i])
+        else:
+            return self.__dict__
+
+    def reload_from_json(self, json):
+        pass
