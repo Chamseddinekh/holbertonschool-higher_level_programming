fcc7267736d9484389cf560b6dba6fa18590045b
chamseddine <chamseddine.khedhri@gmail.com>
2020-12-22 17:55:05 +0100

    rectangle

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""
+class Rectangle that inherits from Base
+"""
+from models.base import Base
+
+
+class Rectangle(Base):
+    """
+    class Rectangle that inherits from Base
+    """
+    def __init__(self, width, height, x=0, y=0, id=None):
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+        super().__init__(id)
+
+    def get_width(self):
+        return self.__width
+
+    def set_width(self, width):
+        self.__width = width
+
+    def get_height(self):
+        return self.__height
+
+    def set_height(self, height):
+        self.__height = height
+
+    def get_x(self):
+        return self.__x
+
+    def set_x(self, x):
+        self.__x = x
+
+    def get_y(self):
+        return self.__y
+
+    def set_y(self, y):
+        self.__y = y
