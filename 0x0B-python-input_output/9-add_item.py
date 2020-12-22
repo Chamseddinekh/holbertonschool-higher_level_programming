45baa41c640a9405dbc055f741ec12234d8cce8e
chamseddine <chamseddine.khedhri@gmail.com>
2020-12-22 01:01:00 +0100

    add

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -3,14 +3,14 @@ import sys
 load_from_json_file = __import__("8-load_from_json_file").load_from_json_file
 save_to_json_file = __import__("7-save_to_json_file").save_to_json_file
 """
-writes an Object to a text file
+script that adds all arguments to a Python list
 """
 
 filename = "add_item.json"
 try:
-    new_list = load_from_json_file(filename)
+    my_list = load_from_json_file(filename)
 except:
-    new_list = []
+    my_list = []
 for i in range(1, len(sys.argv)):
-    new_list.append(sys.argv[i])
-save_to_json_file(new_list, filename)
+    my_list.append(sys.argv[i])
+save_to_json_file(my_list, filename)
