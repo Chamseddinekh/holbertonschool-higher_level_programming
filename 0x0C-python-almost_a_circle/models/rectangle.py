c0c3873dc87ce8440fa8bd929bb0fb49bdc209be
chamseddine <chamseddine.khedhri@gmail.com>
2020-12-22 23:09:06 +0100

    rec

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -20,34 +20,42 @@ class Rectangle(Base):
         self.y = y
         super().__init__(id)
 
+    @property
     def get_width(self):
         """getter of width"""
         return self.__width
 
+    @set_width.setter
     def set_width(self, width):
         """setter of width"""
         self.__width = width
 
+    @property
     def get_height(self):
         """getter of height"""
         return self.__height
 
+    @set_height.setter
     def set_height(self, height):
         """setter of height"""
         self.__height = height
 
+    @property
     def get_x(self):
         """getter of x"""
         return self.__x
 
+    @set_x.setter
     def set_x(self, x):
         """setter of x"""
         self.__x = x
 
+    @property
     def get_y(self):
         """getter of y"""
         return self.__y
 
+    @set_y.setter
     def set_y(self, y):
         """setter of y"""
         self.__y = y
