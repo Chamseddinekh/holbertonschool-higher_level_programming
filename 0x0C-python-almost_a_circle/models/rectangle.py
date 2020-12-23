f8a2910adc40d83674f5cae4cdc94f25d9074d66
chamseddine <chamseddine.khedhri@gmail.com>
2020-12-23 11:17:26 +0100

    rectprintfixed

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -82,11 +82,13 @@ class Rectangle(Base):
 
     def display(self):
         """prints in stdout the Rectangle instance with the character"""
-        for k in range(self.x):
-            print('\n')
+        for k in range(self.y):
+            print("\n",end="")
         for i in range(self.height):
+            for l in range(self.x):
+                    print(" ", end="")
             for j in range(self.width):
-                 print("#", end="")
+                print("#", end="")
             print()
 
     def __str__(self):
