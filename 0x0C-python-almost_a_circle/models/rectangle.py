e5c06528fe4b948c79559dad38f177f21d55b802
chamseddine <chamseddine.khedhri@gmail.com>
2021-01-07 17:14:35 +0100

    kwargsupdate

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -96,7 +96,7 @@ class Rectangle(Base):
         return ("[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.x,
                 self.y, self.width, self.height))
 
-    def update(self, *args):
+    def update(self, *args, **kwargs):
         """update args"""
         if args:
             try:
@@ -107,3 +107,14 @@ class Rectangle(Base):
                 self.y = args[4]
             except:
                 pass
+        else:
+            if 'id' in kwargs:
+                self.id = kwargs['id']
+            if 'width' in kwargs:
+                self.width = kwargs['width']
+            if 'height' in kwargs:
+                self.height = kwargs['height']
+            if 'x' in kwargs:
+                self.x = kwargs['x']
+            if 'y' in kwargs:
+                self.y = kwargs['y']
