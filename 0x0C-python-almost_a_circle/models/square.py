e25f7c529cd0073ac5e131a7765cf43c581dc82c
chamseddine <chamseddine.khedhri@gmail.com>
2021-01-08 13:44:04 +0100

    squaredict

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -52,3 +52,6 @@ class Square(Rectangle):
                     self.x = kwargs['x']
                 if 'y' in kwargs:
                     self.y = kwargs['y']
+
+    def to_dictionary(self): 
+        return {'id': self.id, 'x': self.x, 'size': self.size, 'y': self.y}
