273b36e0310fa592b284954f9ad9bdda86088307
chamseddine <chamseddine.khedhri@gmail.com>
2021-01-08 13:55:24 +0100

    dicttojson

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,6 +2,7 @@
 """
 Base Class
 """
+import json
 
 
 class Base:
@@ -17,3 +18,11 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """dict to json"""
+        if list_dictionaries is None:
+            return '[]'
+        else:
+            return json.dumps(list_dictionaries, sort_keys=True)
