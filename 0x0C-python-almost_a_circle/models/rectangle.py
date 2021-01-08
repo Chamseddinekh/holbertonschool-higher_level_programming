7665268cb5ce2b7645dfd96e389eaaf61ebb50ad
chamseddine <chamseddine.khedhri@gmail.com>
2021-01-08 13:41:28 +0100

    recdict

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -120,4 +120,5 @@ class Rectangle(Base):
                 self.y = kwargs['y']
 
     def to_dictionary(self):
+        """rectangle dict"""
         return{'x': self.x, 'y': self.y, 'id': self.id, 'height': self.height, 'width': self.width}
