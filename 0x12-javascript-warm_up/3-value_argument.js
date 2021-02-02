fdee7b6f10841360cec2bcad7964b1e0bd060422
chamseddine <chamsseddine.khedhri@gmail.com>
2021-02-02 16:04:31 +0100

    argemts value

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -2,8 +2,8 @@
 /* 
  *script that prints the first argument passed to it
  **/
-if (process.argv[2] === undefined) {
-    console.log('No argument');
+if (!process.argv[2]) {
+  console.log('No argument');
 } else {
-    console.log(process.argv[2]);
+  console.log(process.argv[2]);
 }
