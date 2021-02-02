213b3b218889ddb1960e0b4c6c76d3b30f2de4a9
chamseddine <chamsseddine.khedhri@gmail.com>
2021-02-02 16:55:30 +0100

    loop

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+/*
+ *Write a script that prints 3 lines: (like 1-multi_languages.js)
+ *but by using an array of string and a loop
+ **/
+let Tab = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+let i = 0;
+while (Tab[i] != undefined) {
+    console.log(Tab[i]);
+    i++;
+}
