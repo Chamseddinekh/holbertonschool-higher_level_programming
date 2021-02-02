bf487b91fcdd5b0fade9c9f891010c5ea4697aaf
chamseddine <chamsseddine.khedhri@gmail.com>
2021-02-02 22:12:09 +0100

    square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -5,8 +5,8 @@
 if (!parseInt(process.argv[2])) {
   console.log('Missing size');
 } else {
-  x = parseInt(process.argv[2]);
+  let x = parseInt(process.argv[2]);
   for (let i = 0; i < x; i++) 
-    console.log('X'.repeat(x));
+  console.log('X'.repeat(x));
 }
   
