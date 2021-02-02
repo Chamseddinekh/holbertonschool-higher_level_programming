e7755c42f814bdfec974e3b76e51900539338d6c
chamseddine <chamsseddine.khedhri@gmail.com>
2021-02-02 17:15:31 +0100

    occc

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+/*
+ *Write a script that prints 3 lines: (like 1-multi_languages.js)
+ *but by using an array of string and a loop
+ **/
+if (!parseInt(process.argv[2])) {
+  console.log('Missing number of occurrences');
+} else {
+    occ = parseInt(process.argv[2]);
+    for (i = 0; i < occ; i++) {
+        console.log('C is fun');
+    }
+}
