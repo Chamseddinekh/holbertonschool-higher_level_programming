4cc5cd8665108de16b8a18ecdff04c8304be6b68
chamseddine <chamsseddine.khedhri@gmail.com>
2021-03-09 15:30:38 +0100

    recclass

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-/* empty class */
+// class Rectangle
 module.exports = class Rectangle {
-   constructor (w, h) {
-      this.w = w;
-      this.h = h;
-   }
+  constructor (w, h) {
+    this.w = w;
+    this.h = h;
+  }
 };
